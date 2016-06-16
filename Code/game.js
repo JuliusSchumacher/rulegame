501282278125aed6d135a257d93c26cf04475754
Julius Schumacher <juliusschumacher@gmail.com>
2016-06-17 01:27:36 +0200

    0.1.1
    Aestethic chanhes to the rendering engine, basic movement added

--- Code/game.js
+++ Code/game.js
@@ -1,23 +1,26 @@
 function Game(size, charOffset) {
     this.size = size;
     this.charOffset = charOffset;
-    
+
     this.players = [];
-    
+
     this.map = new Map(this.size);
     this.renderer = new Renderer();
-    
+
     this.render = function() {
         this.renderer.map = this.map;
         this.renderer.players = this.players;
-        
+
+        this.renderer.clearAll();
         this.renderer.renderMap(this.charOffset);
         this.renderer.renderPlayers();
     }
 }
 
-$(document).ready(function() {
-    var game = new Game(44, 33);
-    game.players.push(new Player('Julius', [5, 6], 45454));
+var game;
+
+function init() {
+    game = new Game(44, 33);
+    game.players.push(new Player('Julius', [5, 6], 35));
     game.render();
-});
+}
--- Code/player.js
+++ Code/player.js
@@ -3,5 +3,22 @@ var Player = function(name, pos, icon) {
     this.position = pos;
     this.icon = String.fromCharCode(icon);
     
+    this.move = function(direction) {
+        switch(direction) {
+            case 0:
+                this.position[0]--;
+                break;
+            case 1:
+                this.position[1]--;
+                break;
+            case 0:
+                this.position[0]++;
+                break;
+            case 0:
+                this.position[1]++;
+                break;
+        }
+    }
+    
     console.log("New Player " + this.name + " at " + this.position[0] + " " + this.position[1] + " with Icon " + this.icon);
 }
